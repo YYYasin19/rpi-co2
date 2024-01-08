6711a5137ba3834b3203e5f14ccca7b36966b09a
Yasin Tatar <ytatar9@googlemail.com>
2024-01-08 22:10:02 +0100

    new sensor type

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,19 @@
-extern crate serial;
-use serial::core::{BaudRate, CharSize, FlowControl, Parity, PortSettings, StopBits};
-use serial::prelude::*;
-use serial::unix::TTYPort;
+mod sensor;
+use sensor::Sensor;
+
 use std::env;
-use std::io::{Read, Write};
-
-use std::{thread, time::Duration};
-
-use mh_z19::{parse_gas_concentration_ppm, read_gas_concentration};
-
-fn build_port(device: &str) -> Result<TTYPort, serial::Error> {
-    let settings: PortSettings = PortSettings {
-        baud_rate: BaudRate::Baud9600,
-        char_size: CharSize::Bits8,
-        parity: Parity::ParityNone,
-        stop_bits: StopBits::Stop1,
-        flow_control: FlowControl::FlowNone,
-    };
-    let mut port = serial::open(device)?;
-    port.set_timeout(Duration::from_secs(1))?;
-    port.configure(&settings)?;
-    Ok(port)
-}
-
-fn check_port(serial: &mut TTYPort) {
-    match serial.write(&[0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00]) {
-        Ok(_) => println!("Sent test command with 8 bytes"),
-        Err(e) => eprintln!("Failed to send command: {:?}", e),
-    }
-}
 
 fn main() {
     // read from environment variable CO2_DEVICE
     let device = env::var("CO2_DEVICE").unwrap_or("/dev/ttyAMA0".to_string());
-    println!("Reading from device: {}", device);
-    let mut serial = build_port(&device).unwrap();
-    check_port(&mut serial);
 
-    // read from port and write to stdout
-    println!("Reading from port");
-
-    loop {
-        // write command
-        let packet = read_gas_concentration(0x1);
-        match serial.write(&packet) {
-            Ok(_) => println!("Sent [read gas concentration] command"),
-            Err(e) => eprintln!("Failed to send command: {:?}", e),
+    let mut sensor = Sensor::new(&device).unwrap();
+    match sensor.check_port() {
+        true => println!("Port is open 📶"),
+        false => {
+            println!("Port is closed. Quittung!");
+            return;
         }
-
-        // read response
-        let mut response: Vec<u8> = vec![0; 9];
-        match serial.read(&mut response[..]) {
-            Ok(t) => {
-                println!("Read {} bytes", t);
-                let hex_string: Vec<String> =
-                    response.iter().map(|b| format!("{:02x}", b)).collect();
-                println!("Read: {:?}", hex_string);
-                match parse_gas_concentration_ppm(&response) {
-                    Ok(ppm) => println!("CO2: {} ppm", ppm),
-                    Err(e) => eprintln!("Failed to parse response: {:?}", e),
-                }
-            }
-            Err(e) => eprintln!("Failed to read from port: {:?}", e),
-        }
-
-        // sleep a few seconds
-        thread::sleep(Duration::from_secs(1));
     }
+    sensor.read_ppm();
 }
---
+++ src/sensor.rs
@@ -0,0 +1,98 @@
+use serial::unix::TTYPort;
+use serial::SerialPort;
+
+use std::io::{Read, Write};
+use std::time::Duration;
+
+use mh_z19::{calibrate_zero_point, parse_gas_concentration_ppm, read_gas_concentration};
+
+pub struct Sensor {
+    port: TTYPort,
+    serial_device: String, // e.g. /dev/ttyAMA0
+    device_number: u8,     // e.g. 0x1
+}
+
+impl Sensor {
+    pub fn new(device: &str) -> Result<Self, serial::Error> {
+        let settings: serial::PortSettings = serial::PortSettings {
+            baud_rate: serial::BaudRate::Baud9600,
+            char_size: serial::CharSize::Bits8,
+            parity: serial::Parity::ParityNone,
+            stop_bits: serial::StopBits::Stop1,
+            flow_control: serial::FlowControl::FlowNone,
+        };
+        let mut port = serial::open(device)?;
+        port.set_timeout(Duration::from_secs(1))?;
+        port.configure(&settings)?;
+        Ok(Self {
+            port,
+            serial_device: device.to_string(),
+            device_number: 0x1,
+        })
+    }
+
+    /*
+     * This function should send the test command to the sensor
+     */
+    pub fn check_port(&mut self) -> bool {
+        let packet = read_gas_concentration(0x1);
+        match self.port.write(&packet) {
+            Ok(_) => {
+                println!("Sent test command with 8 bytes");
+                true
+            }
+            Err(e) => {
+                eprintln!("Failed to send command: {:?}", e);
+                false
+            }
+        }
+    }
+
+    /*
+     * Reads the gas concentration in ppm from the sensor
+     */
+    pub fn read_ppm(&mut self) -> Option<u32> {
+        let read_gas_cmd = read_gas_concentration(self.device_number);
+        match self.port.write(&read_gas_cmd) {
+            Ok(_) => println!("Sent [read gas concentration] command"),
+            Err(e) => eprintln!("Failed to send command: {:?}", e),
+        }
+
+        // read response
+        let mut response: Vec<u8> = vec![0; 9];
+        match self.port.read(&mut response[..]) {
+            Ok(t) => {
+                println!("Read {} bytes", t);
+                let hex_string: Vec<String> =
+                    response.iter().map(|b| format!("{:02x}", b)).collect();
+                println!("Read: {:?}", hex_string);
+                match parse_gas_concentration_ppm(&response) {
+                    Ok(ppm) => return Some(ppm),
+                    Err(e) => {
+                        eprintln!(
+                            "Failed to parse response: {:?} for {:?}",
+                            e, self.serial_device
+                        );
+                        return None;
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("Failed to read from port: {:?}", e);
+                return None;
+            }
+        }
+    }
+
+    /*
+     * Calibrate the sensor to zero.
+     * Requires the sensor to be in a clean air environment (400ppm) for at least 20 minutes.
+     */
+    pub fn calibrate_zero(&mut self) -> Result<(), serial::Error> {
+        let packet = calibrate_zero_point(0x1);
+        self.port.write(&packet)?;
+        let mut response: Vec<u8> = vec![0; 9];
+        self.port.read(&mut response[..])?;
+        Ok(())
+    }
+}
