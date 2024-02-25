956145ac23f54e8b4da1599c041af79a4022bd0a
Yasin <ytatar9@googlemail.com>
2024-02-25 18:38:43 +0100

    add: dummy data loading + tracing

--- rpi-co2-ui/src/App.tsx
+++ rpi-co2-ui/src/App.tsx
@@ -1,12 +1,11 @@
 import "./App.css";
-
 import { Chart } from "./chart";
 
 function App() {
   return (
     <>
       <div></div>
-      <h1>CO2 Data</h1>
+      <h1>CO2 Data, I guess</h1>
       <Chart />
     </>
   );
--- src/bin/server/mod.rs
+++ src/bin/server/mod.rs
@@ -1,8 +1,13 @@
 use axum::{routing::get, Json, Router};
+use rand::Rng;
 use serde::Serialize;
 use std::{fs::File, io::Read};
+use tower_http::cors::{Any, CorsLayer};
 use tower_http::services::ServeDir;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
+// use tracing_subscriber;
 #[derive(Serialize)]
 struct Co2Data {
     timestamps: Vec<String>,
@@ -31,6 +36,24 @@ async fn data() -> Json<Co2Data> {
     })
 }
 
+async fn dummy_data() -> Json<Co2Data> {
+    let mut rng = rand::thread_rng();
+    let co2values: Vec<i32> = (0..100).map(|_| rng.gen_range(10..100)).collect();
+    let timestamps: Vec<String> = (0..100)
+        .map(|i| {
+            let now = chrono::Utc::now();
+            let dt = now - chrono::Duration::seconds(100 - i);
+            dt.to_rfc3339()
+        })
+        .collect();
+    // print before returning
+
+    Json(Co2Data {
+        timestamps,
+        co2values,
+    })
+}
+
 async fn hello() -> &'static str {
     "Hello world!!"
 }
@@ -41,10 +64,23 @@ async fn echo(req_body: String) -> String {
 
 #[tokio::main]
 async fn main() {
+    // init tracing subscriber so we see logs in the terminal
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let app = Router::new()
         .route("/", get(hello))
         .route("/echo", get(echo))
         .route("/data", get(data))
+        .route("/dummy_data", get(dummy_data))
+        .layer(
+            CorsLayer::new()
+                .allow_origin(Any)
+                .allow_methods(Any)
+                .allow_headers(Any),
+        )
+        .layer(TraceLayer::new_for_http())
         .nest_service("/ui", ServeDir::new("./rpi-co2-ui/dist/"));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
